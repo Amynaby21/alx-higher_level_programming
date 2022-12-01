37955e55358bf31162c80d19962171ee110d13fa
Amynaby21 <aminatandeye15@gmail.com>
2022-12-01 18:14:15 +0100

    Arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n_arg = len(sys.argv) - 1
+    if n_arg == 0:
+        print("{} arguments." .format(n_arg))
+    elif n_arg == 1:
+        print("{} argument:" .format(n_arg))
+    else:
+        print("{} arguments:" .format(n_arg))
+    for i in range(n_arg):
+        print("{}: {}" .format(i + 1, sys.argv[i + 1]))

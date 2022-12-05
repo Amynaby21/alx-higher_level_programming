0194ae09624870215bdaa467734c277c5d7c7f47
Amynaby21 <aminatandeye15@gmail.com>
2022-12-05 21:12:01 +0100

    Divisible by 2

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    muls = []
-    for i in my_list:
-        if i % 2 == 0:
-            muls.append(True)
-        else:
-            muls.append(False)
+    if my_list:
+        muls = []
+        for i in my_list:
+            if i % 2 == 0:
+                muls.append(True)
+            else:
+                muls.append(False)
         return (muls)

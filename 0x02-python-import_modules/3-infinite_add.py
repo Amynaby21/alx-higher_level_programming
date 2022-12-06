e953e93e71ef9e81912bf9dd3132ce2991d394a4
Amynaby21 <aminatandeye15@gmail.com>
2022-12-06 21:05:06 +0100

    Infinte addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    my_sum = 0
-    for i in range(len(sys.argv) - 1):
-        if (len(sys.argv)) > 1:
-            my_sum += (int(sys.argv[i]))
-    print("{}" .format(my_sum))
-
+    print("{:d}" .format(sum(int(i) for i in sys.argv[1:])))

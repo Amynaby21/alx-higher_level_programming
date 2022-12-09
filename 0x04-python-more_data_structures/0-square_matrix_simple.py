533ff1770817a285ab1ef2e36e8f61692f5526dc
Amynaby21 <aminatandeye15@gmail.com>
2022-12-09 21:57:41 +0100

    Square matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if matrix != None:
         print(matrix)
         square_matrix = []
         for i in matrix:
             square_matrix.append(list(map(lambda x: x**2, i)))
         return (square_matrix)
+    return (None)

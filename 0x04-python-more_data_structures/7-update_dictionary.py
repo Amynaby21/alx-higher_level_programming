7e1abf8360723d451a2f9781c688d550bea1e539
Amynaby21 <aminatandeye15@gmail.com>
2022-12-09 23:47:22 +0100

    Update dict

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -3,4 +3,4 @@ def update_dictionary(a_dictionary, key, value):
     if a_dictionary:
         a_dic2 = {key: value}
         a_dictionary.update(a_dic2)
-        return (a_dictionary)
+    return (a_dictionary)

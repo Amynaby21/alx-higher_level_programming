c1f097b704e9db0dcbbe8e2e03a3acd50900e75a
Amynaby21 <aminatandeye15@gmail.com>
2022-12-20 22:10:26 +0100

    New Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines class Square based on 0-square.py"""
+
+
+class Square:
+    """Defines a square"""
+
+
+    def __init__(self, size):
+        """Initialize a new Square
+
+        Agrs:
+            size (int): The size of the new square.
+        """
+        self.__size = size

2a5f363cad1c2d079d6d84e0c0bd7a231a4fb603
Amynaby21 <aminatandeye15@gmail.com>
2022-12-20 14:38:31 +0100

    Function

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from sys import stderr
 
+
 def safe_function(fct, *args):
     try:
         return (fct(*args))
-    except (TypeError, ValueError, IndexError) as e:
+    except (ZeroDivisionError, TypeError, ValueError, IndexError) as e:
         stderr.write("Exception: {}\n" .format(e))
         return None
